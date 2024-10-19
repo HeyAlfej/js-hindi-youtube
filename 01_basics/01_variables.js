efdc6367c8e2f4b04bbbe66fc65d96464c9ebb59
Alfej Mansuri <alfejmansuri1407@gmail.com>
2024-10-19 15:47:59 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountid = 144553
+let accountemail = "alfej@gmail.com"
+var accountpassword = 465697
+accountcity = "Jaipur"
+let accountstate;
+
+// accountid = 3 you cant change it 
+
+accountemail = "alfejmansuri@gmail.com"
+accountpassword = 1472005
+accountcity = "Mumbai"
+
+
+
+console.log(accountid);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountid, accountemail, accountpassword, accountcity, accountstate]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all code js code as newer version
+
+// alert( 3 + 3 ) // we are using node.js , not browser
+
+console.log(3 + 3);
+console.log("Alfej");
+
+
+let name = "Alfej"
+let age = 20
+let isloggedin = false
+let state = null 
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+
+// object
+
+console.log(typeof "Alfej");  
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object
