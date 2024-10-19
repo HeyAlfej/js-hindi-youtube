e55651baa9a85ea63bd2bc98c01addaa13883125
Alfej Mansuri <alfejmansuri1407@gmail.com>
2024-10-19 16:12:31 +0000

    Conversion of Data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score = null
+
+console. log (typeof score);
+console. log (typeof(score));
+
+let valueInNumber = Number(score)
+console. log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
